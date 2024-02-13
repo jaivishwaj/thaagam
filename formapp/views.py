ab06c10653af6ec1b0297f9c48bca49791017a1e
jaivishwaj <jaivishwathaagam@gmail.com>
2024-02-13 11:11:17 +0530

    changes

--- formapp/views.py
+++ formapp/views.py
@@ -1683,7 +1683,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 
 def master_records_dashboard(request):
     if not request.user.is_authenticated:
